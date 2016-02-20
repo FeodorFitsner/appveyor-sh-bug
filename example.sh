2f4df81cb0227543e0278887a55429f23c9b2383
Rory Yorke <rory.yorke@gmail.com>
2016-02-20 21:19:18 +0200

    try a simpler example.

--- example.sh
+++ example.sh
@@ -2,9 +2,9 @@
 set -e
 
 # (should?) fail on Appveyor with MSYS2-MinGW64 with something like
-# ./example.sh: line 10: 0: Bad file descriptor
-test -n "$DJDIR" || exec 7<&0 </dev/null
+# ./example.sh: line 6: 0: Bad file descriptor
+exec 7<&0 </dev/null
 
 # this should fail on all systems with something like
 # ./example.sh: 10: ./example.sh: 3: Bad file descriptor
-test -n "$DJDIR" || exec 7<&3
+exec 7<&3
