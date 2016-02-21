af35759b36c067080553bc1eb5790a961c230baa
Rory Yorke <rory.yorke@gmail.com>
2016-02-21 06:42:54 +0200

    Simpler example.

--- example.sh
+++ example.sh
@@ -3,8 +3,11 @@ set -e
 
 # (should?) fail on Appveyor with MSYS2-MinGW64 with something like
 # ./example.sh: line 6: 0: Bad file descriptor
-exec 7<&0 </dev/null
+echo hello|cat <&0
+
+# should print goodbye
+echo goodbye | (exec 7<&0; cat <&7)
 
 # this should fail on all systems with something like
-# ./example.sh: 10: ./example.sh: 3: Bad file descriptor
-exec 7<&3
+# ./example.sh: line 11: 7: Bad file descriptor
+cat <&7
