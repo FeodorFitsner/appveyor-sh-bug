379091f03618f002e77963994508a3b229f234b8
Rory Yorke <rory.yorke@gmail.com>
2016-02-21 14:07:42 +0200

    More variations on redirection, with expectations.
    Expecting at least the first to fail on Appveyor, not sure about the
    fifth.

--- example.sh
+++ example.sh
@@ -1,16 +1,43 @@
-#! /bin/sh
-set -e
+#! /bin/bash
 
-# (should?) fail on Appveyor with MSYS2-MinGW64 with something like
-# ./example.sh: line 6: 0: Bad file descriptor
-if cat <&0 </dev/null ; then
-    echo "OK"
+exec 3<&0 </dev/null
+if [ $? -eq 0 ]; then
+    echo "OK (no error)"
 else
-    echo "FAIL"
+    echo "FAIL (error)"
 fi
 
-if cat 0<&0 </dev/null ; then
-    echo "OK"
+exec 0<&0 < /dev/null
+if [ $? -eq 0 ]; then
+    echo "OK (no error)"
 else
-    echo "FAIL"
+    echo "FAIL (error)"
+fi
+
+cat 0<&0 </dev/null
+if [ $? -eq 0 ]; then
+    echo "OK (no error)"
+else
+    echo "FAIL (error)"
+fi
+
+exec <&4
+if [ $? -ne 0 ]; then
+    echo "OK (got error)"
+else
+    echo "FAIL (no error)"
+fi
+
+sh -c 'exec 3<&0 < /dev/null'
+if [ $? -eq 0 ]; then
+    echo "OK (no error)"
+else
+    echo "FAIL (got error)"
+fi
+
+bash -c 'exec <&4'
+if [ $? -ne 0 ]; then
+    echo "OK (got error)"
+else
+    echo "FAIL (no error)"
 fi
